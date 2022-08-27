a51876053ee08f8f3623a9115ab48fcd7e0bd360
muhammad-hamza <Muh-Hamza-99>
2022-08-27 19:19:00 +0300

    New route and share live database implemented

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ require("dotenv").config({ path: "./config.env" });
 const express = require("express");
 const app = express();
 
+const share = require("share");
+require("redis");
+
 app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + "/public"));
@@ -10,6 +13,12 @@ app.use(express.static(__dirname + "/public"));
 app.get("/", (req, res) => {
   res.render("pad");
 });
+
+app.get("/:id", (req, res) => {
+    res.render("pad");
+});
+
+share.server.attach(app, { db: { type: "redis" } });
 
 const PORT = process.env.PORT || 8000;
 
